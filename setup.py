9f0ed7c878687f99e3d82270c1909b4316083b7f
Scott Stevenson <scott@stevenson.io>
2018-08-17 00:28:36 +0100

    Format nb-clean and setup.py with Black

--- setup.py
+++ setup.py
@@ -8,30 +8,30 @@ from setuptools import setup
 def read_long_description() -> str:
     """Read from README.md file in root of source directory."""
     root = Path(__file__).resolve().parent
-    readme = root / 'README.md'
-    return readme.read_text(encoding='utf-8')
+    readme = root / "README.md"
+    return readme.read_text(encoding="utf-8")
 
 
 setup(
-    name='nb-clean',
-    version='1.2.0',
-    description='Clean Jupyter notebooks for versioning',
+    name="nb-clean",
+    version="1.2.0",
+    description="Clean Jupyter notebooks for versioning",
     long_description=read_long_description(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/srstevenson/nb-clean',
-    author='Scott Stevenson',
-    author_email='scott@stevenson.io',
-    license='ISC',
+    long_description_content_type="text/markdown",
+    url="https://github.com/srstevenson/nb-clean",
+    author="Scott Stevenson",
+    author_email="scott@stevenson.io",
+    license="ISC",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: ISC License (ISCL)',
-        'Natural Language :: English',
-        'Operating System :: Unix',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Natural Language :: English",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    scripts=['nb-clean'],
-    install_requires=['nbformat'],
-    keywords='jupyter notebook git filter'
+    scripts=["nb-clean"],
+    install_requires=["nbformat"],
+    keywords="jupyter notebook git filter",
 )
