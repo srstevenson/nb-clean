5e0fc2bd670955515de16fd95f289a8bfb1ca38f
Jean-Baptiste Fiot <jean-baptiste.fiot@centraliens.net>
2021-06-19 17:19:27 +0100

    Add test case for clean_notebook

--- tests/test_clean_notebook.py
+++ tests/test_clean_notebook.py
@@ -13,6 +13,16 @@ def test_clean_notebook(
     assert nb_clean.clean_notebook(dirty_notebook) == clean_notebook
 
 
+def test_clean_notebook_with_version(
+    dirty_notebook_with_version: nbformat.NotebookNode,
+    clean_notebook: nbformat.NotebookNode,
+) -> None:
+    """Test nb_clean.clean_notebook with language_info version."""
+    assert (
+        nb_clean.clean_notebook(dirty_notebook_with_version) == clean_notebook
+    )
+
+
 def test_clean_notebook_remove_empty_cells(
     clean_notebook_with_empty_cells: nbformat.NotebookNode,
     clean_notebook: nbformat.NotebookNode,
