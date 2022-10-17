db796b52e334a8ed02ebac317cb328db5d2aeb65
Muhammad Yasirroni <48709672+yasirroni@users.noreply.github.com>
2022-10-17 21:55:34 +0700

    Check for and clean output execution outputs
    These are present on outputs of type `execute_result`, and are now
    checked for by `check_notebook` and set to `null` by `clean_notebook`.
    
    Co-authored-by: Muhammad Yasirroni <48709672+yasirroni@users.noreply.github.com>
    Co-authored-by: Scott Stevenson <scott@stevenson.io>

--- tests/test_check_notebook.py
+++ tests/test_check_notebook.py
@@ -52,16 +52,37 @@ def test_check_notebook_preserve_metadata(
     )
 
 
-@pytest.mark.parametrize("preserve_cell_outputs", [True, False])
+@pytest.mark.parametrize(
+    ("notebook", "preserve_cell_outputs", "is_clean"),
+    [
+        # pylint: disable=no-member
+        (
+            pytest.lazy_fixture("clean_notebook_with_outputs"),  # type: ignore[attr-defined]
+            True,
+            True,
+        ),
+        (
+            pytest.lazy_fixture("clean_notebook_with_outputs"),  # type: ignore[attr-defined]
+            False,
+            False,
+        ),
+        (
+            pytest.lazy_fixture("clean_notebook_with_outputs_with_counts"),  # type: ignore[attr-defined]
+            True,
+            False,
+        ),
+    ],
+)
 def test_check_notebook_preserve_outputs(
-    clean_notebook_with_outputs: nbformat.NotebookNode,
+    notebook: nbformat.NotebookNode,
     preserve_cell_outputs: bool,
+    is_clean: bool,
 ) -> None:
     """Test nb_clean.check_notebook when preserving cell outputs."""
     assert (
         nb_clean.check_notebook(
-            clean_notebook_with_outputs,
+            notebook,
             preserve_cell_outputs=preserve_cell_outputs,
         )
-        is preserve_cell_outputs
+        is is_clean
     )
